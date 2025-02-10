ef5230b658376688cf34303d35d783f4ef0e8c29
A-Bonner <aidanbonner140202@gmail.com>
2025-02-10 18:17:01 -0500

    created joints

--- generate.py
+++ generate.py
@@ -1,28 +1,15 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-x = -2.5
-y = -2.5
-z = 0.5
-for i in range (0,5):
-    for j in range (0,5):
-        for k in range(0,10):
-            pyrosim.Send_Cube(name="Box"+str(i), pos=[x, y, z], size=[length, width, height])
-            bottom = z + (height/2)
-            length = length * 0.9
-            width = width * 0.9
-            height = height * 0.9
-            midpoint = height/2
-            z = bottom + midpoint
-        z = 0.5
-        x = x + 1
-        length = 1
-        width = 1
-        height = 1
-    x = -2.5
-    y = y + 1
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.End()
 
-pyrosim.End()
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-10, 10, 0.5], size=[1, 1, 1])
+    pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,13 +1,18 @@
 import pybullet as p
 import pybullet_data
 import time as t
+from generate import *
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+create_world()
+create_robot()
+
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 for i in range(100000):
     p.stepSimulation()
     t.sleep((1/120))
